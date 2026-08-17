386fc2372f865b1377bdeee97549bc0bb381d971
Rlm484 <rachaellmackinnon@gmail.com>
2026-08-17 13:58:14 +1000

    Testing and Debugging
    With the help of hackclub I added explanations for the ultrasonic sensor code

--- Main/functions.py
+++ Main/functions.py
@@ -1,11 +1,11 @@
 from machine import Pin, time_pulse_us
 import time
 
-SOUND_SPEED=340 # Vitesse du son dans l'air
-TRIG_PULSE_DURATION_US=10
+SOUND_SPEED=340 # Speed of Sound in Air (m/s)
+TRIG_PULSE_DURATION_US=10 # 10ms Pulse
 
-trig_pin = Pin(9, Pin.OUT) # Broche GP15 de la Pico
-echo_pin = Pin(8, Pin.IN)  # Broche GP14 de la Pico
+trig_pin = Pin(9, Pin.OUT) 
+echo_pin = Pin(8, Pin.IN)  
 
 while True:
     # Prepare le signal
@@ -16,8 +16,8 @@ while True:
     time.sleep_us(TRIG_PULSE_DURATION_US)
     trig_pin.value(0)
 
-    ultrason_duration = time_pulse_us(echo_pin, 1, 30000) # Renvoie le temps de propagation de l'onde (en µs)
-    distance_cm = SOUND_SPEED * ultrason_duration / 20000
+    ultrason_duration = time_pulse_us(echo_pin, 1, 30000) # Returns the wave propagation time (in µs)
+    distance_cm = SOUND_SPEED * ultrason_duration / (10000 * 2) # 10000 for micro to centi, times 2 bc there and back
 
     print(f"Distance : {distance_cm} cm")
     time.sleep_ms(500)
